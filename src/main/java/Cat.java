2d8e8cfccf9655a8cebb6846262b22e1d64bd667
Elena_Kuznetsova <alena_k_98@mail.ru>
2021-11-18 21:10:41 +0400

    1

---
+++ src/main/java/Cat.java
@@ -0,0 +1,10 @@
+public class Cat {
+    private String meow;
+
+    public String getMessage() {
+        return meow;
+    }
+    public void setMessage(String message) {
+        this.meow = message;
+    }
+}
